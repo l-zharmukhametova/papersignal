7ae754019be03e8eb1e53a3911adeeb656e6196b
Laura Zharmukhametova <lzharmukhametova@college.harvard.edu>
2019-11-22 13:16:43 -0500

    1

---
+++ interface/app/controllers/articles_controller.rb
@@ -0,0 +1,24 @@
+
+class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+      end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def new
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :text))
+ 
+        if @article.save
+            redirect_to @article
+          else
+            render 'new'
+          end
+    end
+
+end
