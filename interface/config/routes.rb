0fb0d2d0c10108a83180f2eec82b23e8812764e8
Laura Zharmukhametova <lzharmukhametova@college.harvard.edu>
2019-11-21 22:11:20 -0500

    1

--- interface/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  get 'welcome/index'
- 
-  resources :articles
-  
-  root 'welcome#index'
-end
-
-# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
